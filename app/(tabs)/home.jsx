c8d1e74df1b9db7414db5511d313f8c1d8f9d1f4
Luu Minh Thien <128106955+th1enlm02@users.noreply.github.com>
2024-06-11 23:32:28 +0700

    style: update

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { CustomButton } from '../../components'
+import { router } from 'expo-router'
 
 const Home = () => {
   return (
-    <View className="h-full justify-center items-center">
+    <View className="h-full justify-center items-center px-20">
       <Text>Home</Text>
+      <CustomButton 
+        title="Exit"
+        handlePress={() => router.push("/")}
+        textStyles="text-white text-lg"
+        containerStyles="w-full bg-[#245501]"
+      />
     </View>
   )
 }
